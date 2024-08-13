4d15aebfdb66b34c0ade5d82caf7059bdb330d5c
gauravvvvnandan <gaurav07nandan@gmail.com>
2024-08-13 18:41:33 +0530

    Update diffie-hellman.c

--- diffie-hellman.c
+++ diffie-hellman.c
@@ -1,32 +1,36 @@
-#include<stdio.h>
-long long int power(long long int a, long long int b,
-long long int P)
-{
-if (b == 1)
-return a;
-else
-return (((long long int)pow(a, b)) % P);
+#include <math.h>
+#include <stdio.h>
+
+long long int power(long long int a, long long int b, long long int p) {
+    if (b == 1)
+        return a % p;
+    else
+        return ((long long int)pow(a, b) % p);
 }
-int main()
-{
-long long int P, G, x, a, y, b, ka, kb;
-printf("Enter the value of prime number P: ");
-scanf("%lld", &P);
-printf("Enter the value of primitive root G: ");
-scanf("%lld", &G);
-printf("Enter the private key for user A (a): ");
-scanf("%lld", &a);
-printf("Enter the private key for user B (b): ");
-scanf("%lld", &b);
-printf("\nThe value of P : %lld\n", P);
-printf("The value of G : %lld\n\n", G);
-printf("The private key a for user A : %lld\n", a);
-printf("The private key b for user B : %lld\n\n", b);
-x = power(G, a, P); 
-y = power(G, b, P); 
-ka = power(y, a, P); 
-kb = power(x, b, P); 
-printf("Secret key for User A is : %lld\n", ka);
-printf("Secret key for user B is : %lld\n", kb);
-return 0;
+
+int main() {
+    long long int p, g, x, a, y, b, ka, kb;
+
+    printf("Enter the value of p (Prime Number): ");
+    scanf("%lld", &p);
+
+    printf("Enter the value of g (A generator of p): ");
+    scanf("%lld", &g);
+
+    printf("Enter the value of a private key for Alice: ");
+    scanf("%lld", &a);
+
+    printf("Enter the value of a private key for Bob: ");
+    scanf("%lld", &b);
+
+    x = power(g, a, p);
+    y = power(g, b, p);
+
+    ka = power(y, a, p);
+    kb = power(x, b, p);
+
+    printf("Secret key for Alice is : %lld\n", ka);
+    printf("Secret key for Bob is : %lld\n", kb);
+
+    return 0;
 }
