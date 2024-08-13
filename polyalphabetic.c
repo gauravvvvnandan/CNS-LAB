acab42ad555651686745f0f4bb58b276086d6afb
gauravvvvnandan <gaurav07nandan@gmail.com>
2024-08-13 06:06:20 +0530

    Update polyalphabetic.c

--- polyalphabetic.c
+++ polyalphabetic.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
 void vigenereCipher(char message[], char key[], char result[], int encrypt) {
     int msgLen = strlen(message);
     int keyLen = strlen(key);
     int i, j;
-
     for (i = 0, j = 0; i < msgLen; i++) {
         if (isalpha(message[i])) {
             char base;
@@ -28,25 +26,17 @@ void vigenereCipher(char message[], char key[], char result[], int encrypt) {
     }
     result[msgLen] = '\0';
 }
-
 int main() {
     char message[1000], key[100], encryptedMessage[1000], decryptedMessage[1000];
-
     printf("Enter the message to encrypt: ");
     fgets(message, sizeof(message), stdin);
-    message[strcspn(message, "\n")] = '\0'; // Remove the newline character from input
-
+    message[strcspn(message, "\n")] = '\0';
     printf("Enter the encryption key: ");
     fgets(key, sizeof(key), stdin);
-    key[strcspn(key, "\n")] = '\0'; // Remove the newline character from input
-
-    // Encrypt the message
+    key[strcspn(key, "\n")] = '\0';
     vigenereCipher(message, key, encryptedMessage, 1);
     printf("Encrypted Message: %s\n", encryptedMessage);
-
-    // Decrypt the encrypted message
     vigenereCipher(encryptedMessage, key, decryptedMessage, 0);
     printf("Decrypted Message: %s\n", decryptedMessage);
-
     return 0;
 }
